3a4965a112f7e092af4f76c3e4bc633142a38eba
lihonghua <243573295@qq.com>
2017-12-07 17:39:44 +0800

    添加测试

---
+++ src/main/java/com/lee/wangel/Test.java
@@ -0,0 +1,13 @@
+package com.lee.wangel;
+
+/**
+ * @Desription: 测试
+ * @CreatedDate: 2017/12/7 17:34
+ * @Author：wangel.lee
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+    }
+}
